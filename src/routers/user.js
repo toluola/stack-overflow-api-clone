839351fc9e3b818eede583f55ec6ea9bb7057df5
Tolulope Olaniyan <tolulopeolaniyan@Tolulopes-MacBook-Pro.local>
2020-03-02 16:10:29 +0100

    Implement User Signup

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,7 +1,18 @@
 import { Router } from "express";
 import { createValidationFor, checkValidationResult } from "../utils/validator";
+import { createUser } from "../controllers/user";
 
 const router = Router();
+
+/**
+ * Resource handling user signup
+ * @name router:/signup
+ * @function
+ * @memberof module:Express.Router
+ * @inner
+ * @param {function} createUser - Express path
+ * @returns Response Object
+ */
 
 router.post(
   "/signup",
